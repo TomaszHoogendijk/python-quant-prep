2c3fa8cdfda3f593bf595370d653417d23581bce
Tomasz <Tomasz.hoogendijk@outlook.com>
2026-07-14 00:44:48 +0200

    Add NumPy room assignment simulation

---
+++ probability_experiments/room_assignment_numpy.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def room_assignment(num_rooms: int, num_people: int, assignment: list[int], num_trials: int) -> float:
+    if (len(assignment) != num_rooms or sum(assignment) != num_people or num_people<1 or num_rooms<1 
+        or num_trials<1 or any(value<0 for value in assignment)):
+        raise ValueError("Invalid input")
+    assignment_shape = (num_trials, num_people)
+    outcomes = np.random.randint(1, num_rooms+1, size=assignment_shape)
+    room_counts = []
+    for room in range(1, num_rooms+1):
+        room_counts.append((outcomes==room).sum(axis=1))
+    occupancy_matrix = np.array(room_counts).T
+    sorted_occupancy = np.sort(occupancy_matrix, axis=1)
+    sorted_assignment = np.sort(assignment)
+    matches = (sorted_occupancy == sorted_assignment).all(axis=1)
+    successful_trials = matches.sum()   
+    return successful_trials/num_trials
+
+
+if __name__ == "__main__":
+    result = room_assignment(4,8,[3,3,1,1],1000000)
+    print(result)
